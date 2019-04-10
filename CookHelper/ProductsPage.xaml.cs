95cf458a8f87d942daf6035e0e78891478ba8ee5
achorostian <andrzejchorostian@outlook.com>
2019-04-10 14:30:57 +0200

    Add show/hide animation to SearchBar

--- CookHelper/ProductsPage.xaml.cs
+++ CookHelper/ProductsPage.xaml.cs
@@ -8,8 +8,6 @@ using Xamarin.Forms;
 
 namespace CookHelper
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
     public partial class ProductsPage : ContentPage
     {
@@ -17,5 +15,24 @@ namespace CookHelper
         {
             InitializeComponent();
         }
+
+        async void SearchBarHide(object sender, System.EventArgs e)
+        {
+            searchBox.Unfocus();
+            await Task.WhenAll(
+                searchBar.TranslateTo(0, -44, 300, Easing.CubicOut),
+                defaultBar.TranslateTo(0, 0, 300, Easing.CubicOut)
+            );
+        }
+
+        async void SearchBarShow(object sender, System.EventArgs e)
+        {
+            searchBox.Focus();
+            await Task.WhenAll(
+                searchBar.TranslateTo(0, 0, 300, Easing.CubicOut),
+                defaultBar.TranslateTo(0, 44, 300, Easing.CubicOut)
+            );
+        }
+
     }
 }
