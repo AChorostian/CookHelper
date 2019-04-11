37f98255673e566d713e3f9e0def4fed4d8624a4
achorostian <andrzejchorostian@outlook.com>
2019-04-11 10:58:41 +0200

    Add CustomTabBar

---
+++ CookHelper/Views/CustomTabBar.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CookHelper.Views
+{
+    public partial class CustomTabBar : Grid
+    {
+        public CustomTabBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
