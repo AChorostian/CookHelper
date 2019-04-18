6963b5f89a7337739bdf60f8f7097348a50bf5ce
AChorostian <andrzejchorostian@outlook.com>
2019-04-18 22:45:22 +0200

    Add Recipe model and optional imagesource to Product model

---
+++ CookHelper/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace CookHelper.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        //public string ImageSource { get; set; }
+
+        public string Description { get; set; }
+        public double Multiplier { get; set; }
+
+        public ObservableCollection<Product> Products { get; set; }
+    }
+}
